b2e74a66497ec551c54a1c7df179d978b7c24c34
johan.norberg99@hotmail.com <70860437+N0bba@users.noreply.github.com>
2025-05-29 20:33:14 +0200

    Klar

--- uppg10.js
+++ uppg10.js
@@ -16,7 +16,7 @@ let results = {
 }
   // För varje nummer i arrayen
     for (number of numbers) {
-      // Om numret delas med två och RESTEN av delningen ÄR likamed 0 OCH numret är MER eller likamed än 10.
+      // Om numret delas med två och RESTEN av delningen ÄR likamed 0 OCH numret är MER än 10.
       if (number % 2 == 0 && number > 10)  {
         // Lägg till numret i arrayen bigEven i objektet results
         results.bigEven.push(number);
